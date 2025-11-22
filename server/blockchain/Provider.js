13a165a85eed1069ba8fb619f26617e3b50fe292
Vedant45 <vedantshah5445@gmail.com>
2025-11-22 18:04:46 +0530

    web3 instance added

---
+++ server/blockchain/Provider.js
@@ -0,0 +1,16 @@
+// blockchain/provider.js
+import Web3 from "web3";
+
+let web3;
+
+export const createWeb3Instance = () => {
+    try {
+        web3 = new Web3(new Web3.providers.HttpProvider(process.env.RPC_URL));   
+        console.log("Web3 instance created successfully");
+    } catch (error) {
+        console.log("Web3 instance creation error: ",error);
+    }
+}
+
+
+export default web3;
